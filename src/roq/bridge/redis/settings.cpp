c5113c4bbe12065fcf0b04e3bfb449675055c3ed
thraneh <thraneh@gmail.com>
2023-07-03 16:30:09 +0200

    Use Settings (#356)

--- src/roq/bridge/redis/settings.cpp
+++ src/roq/bridge/redis/settings.cpp
@@ -6,6 +6,8 @@
 
 #include "roq/bridge/redis/flags/flags.hpp"
 
+#include "roq/client/flags/settings.hpp"
+
 using namespace std::literals;
 
 namespace roq {
@@ -13,7 +15,7 @@ namespace bridge {
 namespace redis {
 
 Settings::Settings(client::Type type)
-    : client::Settings2{client::create_settings(type)}, exchange{flags::Flags::exchange()},
+    : client::Settings2{client::flags::create_settings(type)}, exchange{flags::Flags::exchange()},
       symbol{flags::Flags::symbol()}, redis_address{flags::Flags::redis_address()},
       redis_port{flags::Flags::redis_port()}, mbp_depth{flags::Flags::mbp_depth()} {
   log::debug("settings={}"sv, *this);
